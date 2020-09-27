9830df818e23d8d6ed4999f73b68900993851a37
Boris Gomolka <boris.gomolka@gameduell.de>
2020-09-27 02:56:26 +0200

    page2: device macros working. Working

--- Controller Scripts/bgom/nocturn.control.js
+++ Controller Scripts/bgom/nocturn.control.js
@@ -1,11 +1,24 @@
 
 loadAPI(1);
 
-host.defineController("Novation", "Automap MIDI Nocturn", "1.0", "DCA77860-FDCC-11EA-8B6E-0800200C9A66");
+host.defineController("bgom", "Automap MIDI Nocturn", "1.0", "DCA77860-FDCC-11EA-8B6E-0800200C9A66");
 host.defineMidiPorts(1, 1);
 host.addDeviceNameBasedDiscoveryPair(["Automap MIDI"], ["Automap MIDI"]);
 
 var states = { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0 };
+
+var page_states = [
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 127, 31: 0, 32: 0, 33: 0, 34: 0, 35: 0, 36: 0, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 0, 31: 127, 32: 0, 33: 0, 34: 0, 35: 0, 36: 0, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 128, 31: 0, 32: 127, 33: 0, 34: 0, 35: 0, 36: 0, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 128, 31: 0, 32: 0, 33: 127, 34: 0, 35: 0, 36: 0, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 128, 31: 0, 32: 0, 33: 0, 34: 127, 35: 0, 36: 0, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 128, 31: 0, 32: 0, 33: 0, 34: 0, 35: 127, 36: 0, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 128, 31: 0, 32: 0, 33: 0, 34: 0, 35: 0, 36: 127, 37: 0 },
+    { 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 30: 128, 31: 0, 32: 0, 33: 0, 34: 0, 35: 0, 36: 0, 37: 127 },
+]
+
+    ;
 
 
 var MODE_PAGE =
@@ -70,9 +83,8 @@ function init() {
 
         track.getVolume().addValueObserver(128, makeIndexedFunction(t, function (index, value) {
             println("Volume: track: " + index + " value: " + value);
-            if (current_page == MODE_PAGE.MIXER) {
-                states[20 + index] = value
-            }
+                //states[20 + index] = value
+                page_states[MODE_PAGE.MIXER][20 + index] = value;
         })
 
         );
@@ -90,9 +102,8 @@ function init() {
     for (var p = 0; p < 8; p++) {
         var macro = primaryInstrument.getMacro(p).getAmount();
         macro.addValueObserver(128, makeIndexedFunction(p, function (index, value) {
-            if (current_page == MODE_PAGE.DEVICE) {
-                states[20 + index] = value
-            }
+            //states[20 + index] = value
+            page_states[MODE_PAGE.DEVICE][20 + index] = value
         }));
     }
 }
@@ -110,12 +121,14 @@ function pausecomp(millis) {
 function flush() {
     var index;
     //println("page " + current_page );
-    for (var key in states) {
+    for (var key in page_states[current_page]) {
         //pausecomp(200);
 
         //println("flush() key: " + key + " states[key]: " + states[key]);
 
-        sendChannelController(0, key, states[key]);
+        //sendChannelController(0, key, states[key]);
+        sendChannelController(0, key, page_states[current_page][key]);
+
     }
 
 }
@@ -128,6 +141,9 @@ function onMidi(status, data1, data2) {
         println("onMidi() encoder data");
         if (current_page == MODE_PAGE.MIXER)
             trackBank.getTrack(CC_ENCODER.indexOf(data1)).getVolume().set(data2, 128);
+        else if (current_page == MODE_PAGE.DEVICE)
+            var macro = primaryInstrument.getMacro(CC_ENCODER.indexOf(data1)).getAmount().set(data2, 128);
+
     }
     else if (CC_FADER.indexOf(data1) > -1) {
         println("onMidi() CC_FADER.data1: " + CC_FADER.indexOf(data1));
@@ -139,12 +155,14 @@ function onMidi(status, data1, data2) {
         println("onMidi() button data");
         current_page = CC_BUTTON.indexOf(data1);
         for (var key in CC_BUTTON) {
-            states[CC_BUTTON[key]] = 0;
+            //states[CC_BUTTON[key]] = 0;
+            page_states[current_page][CC_BUTTON[key]] = 0;
+
         }
     }
 
-    states[data1] = data2;
-
+    //states[data1] = data2;
+    page_states[current_page][data1] = data2;
 }
 
 function onSysex(data) {
